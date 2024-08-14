02e3e6bd79a0a18ededc179710342b52dde75415
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-08-14 09:05:58 +0530

    Update ScoreCard.java

--- ScoreCard.java
+++ ScoreCard.java
@@ -1,3 +1,8 @@
 public class ScoreCard{
 // Complete Your Code Here
+  // Use Scanner
+  // Use && Operator
+  // Use Operators >= && <=
+  // ID Name Score 
+  // Calculate Grade and Print Grade
 }
